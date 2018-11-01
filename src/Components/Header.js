e378f8b7e43212826c605426a3cc7b4f682bb27a
Jasmin Trotter <jasminmtrotter@gmail.com>
2018-11-01 01:34:33 -0700

    social logos

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
-import logo from '../images/logo.jpg';
+import logo from '../assets/images/logo.jpg';
+import ig from '../assets/images/ig.png';
+import fb from '../assets/images/fb.png';
 
 export default function Header(props) {
 	return(
 		<header className="App-header">
           <img src={logo} className="App-logo" alt="ballet body by jasmin logo" />
-          
+          <div className="social-div">
+	          <a href="https://www.facebook.com/balletbodybyjasmin" target="_blank"><img src={fb} className="social-logo" alt="facebook link" /></a>
+	          <a href="https://www.instagram.com/balletbodybyjasmin/" target="_blank"><img src={ig} className="social-logo" alt="instagram link" /></a>
+          </div>
         </header>
 	);
 }
