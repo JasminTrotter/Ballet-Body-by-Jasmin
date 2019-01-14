f3aa1c01d66d202b0d4fe1b94a073be92cdd00d9
Jasmin Trotter <jasminmtrotter@gmail.com>
2019-01-14 03:07:46 -0800

    calendar

---
+++ src/Components/ClassPanel.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import MoreInfo from './MoreInfo';
+
+export default class ClassPanel extends React.Component {
+    constructor(props) {
+		super(props);
+		this.state = {
+            isHidden: true,
+            hideLabel: false
+		}
+    }
+
+    componentDidMount() {
+        window.scrollTo(0, 0)
+    }
+    
+    toggleHidden() {
+        this.setState({
+          isHidden: !this.state.isHidden,
+          hideLabel: !this.state.hideLabel
+        })
+      }
+
+    render() {
+
+        const label = this.state.hideLabel ? 'Hide' : 'Read More';
+
+        return (
+
+                    <div className="class-panel class-panel1">
+						<h3>{this.props.header}</h3>
+						<h4>{this.props.subHeader}</h4>
+						
+						<h5 style={{"color":"red"}}>All classes require prior reservation.</h5>
+						<h5>email jasmin@balletbodybyjasmin.com</h5>
+
+						<h6>Description:</h6>
+						<p>
+							{this.props.description}
+						</p>
+                        <button className="naving-button" onClick={this.toggleHidden.bind(this)}>{label}</button>
+                        <div>
+                            {!this.state.isHidden && <MoreInfo more1={this.props.more1} more2={this.props.more2}/> }
+                        </div>
+                        
+						
+					</div>
+
+        );
+        
+    }
+}
